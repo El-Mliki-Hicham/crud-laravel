8bad54256f98b7e54cf70114f6a8ad5f63289e9a
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-05-14 13:26:06 +0100

    crud

--- database/migrations/2022_05_14_121725_make_usre_id_nullable_on_posts_table.php
+++ database/migrations/2022_05_14_121725_make_usre_id_nullable_on_posts_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('crud_laravels', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();;
-            $table->string('email')->nullable();;
-            $table->string('message')->nullable();;
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -29,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('crud_laravels');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 };
